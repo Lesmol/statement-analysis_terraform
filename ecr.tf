a89e50e40308385a435d2b6886a40b0940cf4b11
Lesedi <molemilesedi@gmail.com>
2026-05-10 17:09:07 +0200

    Add ECR repository policy to allow Lambda access for image retrieval

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_ecr_repository" "statement_analysis" {
   name = "statement-analysis"
   image_tag_mutability = "MUTABLE"
@@ -6,4 +8,30 @@ resource "aws_ecr_repository" "statement_analysis" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+resource "aws_ecr_repository_policy" "statement_analysis" {
+  repository = aws_ecr_repository.statement_analysis.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+        Condition = {
+          StringLike = {
+            "aws:sourceArn" = "arn:aws:lambda:*:${data.aws_caller_identity.current.account_id}:function:*"
+          }
+        }
+      }
+    ]
+  })
 }
