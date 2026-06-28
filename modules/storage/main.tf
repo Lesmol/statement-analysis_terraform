3e2fe673d1c58c9a4131e979ac4532dbb4cac13c
Lesedi <molemilesedi@gmail.com>
2026-06-28 12:51:01 +0200

    chore: add bucket policy for textract to read from s3

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,4 +1,19 @@
 resource "aws_s3_bucket" "statement_analysis_docs" {
-  bucket = var.bucket_name
+  bucket   = var.bucket_name
   provider = aws.textract-region
+}
+
+resource "aws_s3_bucket_policy" "statement_analysis_textract_read" {
+  bucket = aws_s3_bucket.statement_analysis_docs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "TextractReadAccess"
+      Effect    = "Allow"
+      Principal = { Service = "textract.amazonaws.com" }
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.statement_analysis_docs.arn}/*"
+    }]
+  })
 }
