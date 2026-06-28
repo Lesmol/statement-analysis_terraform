5c8b5cedbda94c90e791da00b57aa1f70da77b2b
Lesedi <molemilesedi@gmail.com>
2026-06-28 14:01:55 +0200

    add Cognito

---
+++ modules/auth/main.tf
@@ -0,0 +1,49 @@
+resource "aws_cognito_user_pool" "statement_analysis" {
+  name = var.user_pool_name
+
+  password_policy {
+    minimum_length    = 8
+    require_uppercase = true
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = false
+  }
+
+  auto_verified_attributes = ["email"]
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    required            = true
+    mutable             = true
+  }
+
+  schema {
+    name                = "phone_number"
+    attribute_data_type = "String"
+    required            = false
+    mutable             = true
+  }
+}
+
+resource "aws_cognito_user_pool_client" "statement_analysis" {
+  name         = var.user_pool_client_name
+  user_pool_id = aws_cognito_user_pool.statement_analysis.id
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
+
+  access_token_validity  = 1
+  id_token_validity      = 1
+  refresh_token_validity = 30
+
+  generate_secret = false
+}
---
+++ modules/auth/variables.tf
@@ -0,0 +1,9 @@
+variable "user_pool_name" {
+  type    = string
+  default = "statement-analysis-user-pool"
+}
+
+variable "user_pool_client_name" {
+  type    = string
+  default = "statement-analysis-client"
+}
