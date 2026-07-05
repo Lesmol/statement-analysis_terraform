a2799ab281ac96ae992c688898daea8e1ec7abea
Lesedi <molemilesedi@gmail.com>
2026-07-05 14:55:23 +0200

    chore: provide amplify a role

--- modules/amplify/main.tf
+++ modules/amplify/main.tf
@@ -1,8 +1,29 @@
+data "aws_iam_policy_document" "amplify_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["amplify.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "amplify" {
+  name               = "statement-analysis-amplify-role"
+  assume_role_policy = data.aws_iam_policy_document.amplify_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "amplify" {
+  role       = aws_iam_role.amplify.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
+}
+
 resource "aws_amplify_app" "statement_analysis" {
   name       = var.app_name
   repository = var.repository_url
 
-  access_token = var.github_access_token
+  iam_service_role_arn = aws_iam_role.amplify.arn
+  access_token         = var.github_access_token
 
   build_spec = <<-EOT
     version: 1
